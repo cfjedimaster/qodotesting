0e673f7d7fc95f8ae2ac59225e6903f1f1603e26
Raymond Camden <raymondcamden@gmail.com>
2025-04-10 11:07:45 -0500

    m

---
+++ pirateweather.js
@@ -0,0 +1,12 @@
+let pirate_api_key = process.env.PIRATE_KEY;
+let lat = 30.216;
+let lng = -92.033;
+
+
+async function getPirateWeather(lat, lng, key) {
+    let resp = await fetch(`https://api.pirateweather.net/forecast/${pirate_api_key}/${lat},${lng}?units=us&exclude=currently,minutely,hourly,alerts`);
+	return await resp.json();
+}
+
+let results = await getPirateWeather(lat, lng, pirate_api_key);
+console.log(results);
